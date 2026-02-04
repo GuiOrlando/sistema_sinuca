3decccc5c2d8b76313db36e4a2a94771856ccb0f
Guilherme <guilherme.sinapse.ped@gmail.com>
2026-02-04 09:05:42 -0300

    Novo favicon

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -3,6 +3,7 @@
 import { usePathname } from 'next/navigation';
 import Sidebar from '@/components/Sidebar';
 import './globals.css';
+// import Head from 'next/head';
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
@@ -10,6 +11,12 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="pt-br">
+      <head>
+        <title>SinuControl</title>
+        <link rel="icon" href="/logo-sc.jpg" />
+        <link rel="shortcut icon" href="/logo-sc.jpg" />
+        <link rel="apple-touch-icon" href="/logo-sc.jpg" />
+      </head>
       <body className={`flex bg-background text-foreground antialiased ${isLoginPage ? 'justify-center items-center' : ''}`}>
         {!isLoginPage && <Sidebar />}
         <main className={`${isLoginPage ? 'w-full' : 'flex-1 p-8 overflow-y-auto'}`}>
